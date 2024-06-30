b4b7f33d0a5a7a22ee77d9bd4bc062e2adca53a5
heydocode <169077757+heydocode@users.noreply.github.com>
2024-06-30 13:37:52 +0200

    Setup Wizard
    Added a Setup Wizard to help complete beginners to use this software

--- src/file_writer.rs
+++ src/file_writer.rs
@@ -1,5 +1,7 @@
 use std::io::prelude::*;
 use std::fs::OpenOptions;
+use std::path::Path;
+use std::fs::File;
 
 pub fn write_to_file(text: String, file_path: &str) -> Result<(), String> {
 
@@ -10,4 +12,24 @@ pub fn write_to_file(text: String, file_path: &str) -> Result<(), String> {
         .map_err(|e| e.to_string())?;
 
     write!(file, "{} ", text).map_err(|e| e.to_string())
+}
+
+pub fn update_credentials(username: String, password: String) {
+    let file_path = "config/smtp_account.txt";
+    let path = Path::new(file_path);
+
+    let mut content = String::new();
+    {
+        let mut file = File::open(path).expect("An error occurs when tried to open a file");
+        file.read_to_string(&mut content).expect("An error occurs when tried to read the file");
+    }
+
+    let updated_content = content
+        .replace("username:example@gmail.com", &format!("username:{}", username))
+        .replace("password:password123123", &format!("password:{}", password));
+
+    {
+        let mut file = OpenOptions::new().write(true).truncate(true).open(&path).expect("An error occurs when tried to open a file with OpenOptions");
+        file.write_all(updated_content.as_bytes()).expect("An error occurs when tried to update content");
+    }
 }
--- src/setup.rs
+++ src/setup.rs
@@ -1,10 +1,19 @@
 use std::path::Path;
 use std::fs::OpenOptions;
-use std::io::prelude::*;
+use std::io::*;
+use std::io;
 
 use crate::file_reader::smtp_account_reader;
+use crate::file_writer::update_credentials;
 
-pub fn check_config_components() -> Result<(), String> {
+fn read_line() -> String {
+    let mut input = String::new();
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    input.trim().to_string()
+}
+
+pub fn check_config_components() -> std::result::Result<(), String> {
     let components = vec![
         Path::new("config/logs.txt"),
         Path::new("config/smtp_account.txt"),
@@ -24,11 +33,68 @@ pub fn check_config_components() -> Result<(), String> {
             match smtp_account_reader() {
                 Err(_) => {
                     let _ = write!(file, "username:example@gmail.com\npassword:password123123\n\n### The username is your gmail account (example@gmail.com)\n### The password is a 'password for apps' created in Google\n## NOTICE: To create an app-password, you have to have 2-step verification\n## enabled and maybe all rescue options of saving the account.").map_err(|e| e.to_string());
-                    panic!("\n\nThe config has any valid information! Please check it out in config/smtp_account.txt!\n\n");
+                    setup_wizard();
                 },
                 _ => {}
             }
         }
     }
     Ok(())
+}
+
+pub fn setup_wizard() {
+    let mut iter: u8 = 1;
+    let mut smtp_account_username = String::new();
+    let mut smtp_account_password = String::new();
+
+    loop {
+        match iter {
+            1 => {
+                println!("Enter your smtp account username:");
+            },
+            2 => {
+                println!("Can you confirm your smtp account username (Y or N)?");
+            },
+            3 => {
+                println!("Enter your smtp account password:");
+            },
+            4 => {
+                println!("Can you confirm your smtp account password (Y or N)?");
+            },
+            _ => ()
+        }
+        let input = read_line();
+        match iter {
+            1 => {
+                println!("You have entered \"{}\" as your smtp account username", input);
+                smtp_account_username = input.clone();
+                iter += 1;
+            },
+            2 => {
+                if input.to_lowercase() == "y" {
+                    iter += 1;
+                }
+                else {
+                    iter = 1
+                }
+            },
+            3 => {
+                println!("You have entered \"{}\" as your smtp account password", input);
+                smtp_account_password = input.clone();
+                iter += 1;
+            },
+            4 => {
+                if input.to_lowercase() == "y" {
+                    iter += 1;
+                }
+                else {
+                    iter = 3
+                }
+            }
+            _ => {
+                update_credentials(smtp_account_username, smtp_account_password);
+                return;
+            }
+        }
+    }
 }
